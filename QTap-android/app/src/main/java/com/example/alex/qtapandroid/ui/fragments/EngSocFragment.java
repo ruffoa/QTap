ef81d1f6c90ebf566cec0aa59522551b8e5338b3
Carson Cook <cook.carson38@gmail.com>
2017-02-20 00:41:29 -0500

    cleaned up placeholder fragments

--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/ui/fragments/EngSocFragment.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/ui/fragments/EngSocFragment.java
@@ -17,6 +17,6 @@ public class EngSocFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_engsoc,container,false);
+        return inflater.inflate(R.layout.fragment_engsoc, container, false);
     }
 }
--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/ui/fragments/InformationFragment.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/ui/fragments/InformationFragment.java
@@ -13,31 +13,10 @@ import com.example.alex.qtapandroid.R;
 
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link InformationFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link InformationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class InformationFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_information, container, false);
-
     }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        TextView dataInfo = (TextView) getView().findViewById(R.id.dataInfo2);
-//        dataInfo.setText("Initializing Database");
-//
-    }
-
-    public void setup() {
-    }
-
-
 }
