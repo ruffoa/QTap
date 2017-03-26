f4d104144670359a0b2d12b0b0cc24725491029e
ruffoa <ruffo.alex@gmail.com>
2017-03-26 15:47:42 -0400

    Added Google Maps API stuff, and implemented basic functionality in the map

--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/MapsActivity.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/activities/MapsActivity.java
@@ -2,17 +2,22 @@ package com.example.alex.qtapandroid.activities;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.example.alex.qtapandroid.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.alex.qtapandroid.R;
+
+import java.security.Permission;
+
+import static java.security.AccessController.getContext;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -21,7 +26,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps2);
+        setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -42,27 +47,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            int MY_PERMISSIONS_REQUEST_LOCATION = 0;
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    MY_PERMISSIONS_REQUEST_LOCATION);
-
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(true);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(44.228185, -76.492447);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("ILC :)"));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+                new LatLng(44.228185, -76.492447)).zoom(16).build();
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(44.224625, -76.497790)).title("Stirling");
+        marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        // adding marker
+        mMap.addMarker(marker);
+
+
+        PackageManager pm = this.getApplicationContext().getPackageManager();   // check if permission was granted for user location
+        int fineLoc = pm.checkPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                this.getApplicationContext().getPackageName());
+        int corseLoc = pm.checkPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                this.getApplicationContext().getPackageName());
+        if (fineLoc == PackageManager.PERMISSION_GRANTED || corseLoc == PackageManager.PERMISSION_GRANTED);
+            mMap.setMyLocationEnabled(true); // false to disable
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
