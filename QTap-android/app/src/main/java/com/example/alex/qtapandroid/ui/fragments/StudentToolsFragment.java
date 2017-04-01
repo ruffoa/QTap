279dc63854b806f4e9574c9c8a44f855430517e8
LachlanDevir <14lad3@queensu.ca>
2017-03-31 21:00:23 -0400

    Added functionality to student tools page
    Need to test if link is correct

--- QTap-android/app/src/main/java/com/example/alex/qtapandroid/ui/fragments/StudentToolsFragment.java
+++ QTap-android/app/src/main/java/com/example/alex/qtapandroid/ui/fragments/StudentToolsFragment.java
@@ -1,11 +1,15 @@
 package com.example.alex.qtapandroid.ui.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.alex.qtapandroid.R;
 import com.example.alex.qtapandroid.ICS.ParseICS;
@@ -23,6 +27,38 @@ public class StudentToolsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_student_tools, container, false);
+        View v = inflater.inflate(R.layout.fragment_student_tools, container, false);
+        Button solus = (Button) v.findViewById(R.id.solus_button);
+        Button outlook = (Button) v.findViewById(R.id.outlook_button);
+        Button onq = (Button) v.findViewById(R.id.onq_button);
+        solus.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.queensu.ca/"));
+                startActivity(intent);
+            }
+        });
+        outlook.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://login.microsoftonline.com/"));
+                startActivity(intent);
+            }
+        });
+        onq.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.queensu.ca/"));
+                startActivity(intent);
+            }
+        });
+
+        return v;
     }
 }
